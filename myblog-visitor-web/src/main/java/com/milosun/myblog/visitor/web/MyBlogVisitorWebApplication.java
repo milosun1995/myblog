aa6f21986b8f50b2b78376355051339f619b5237
milosun1995 <18838980210@163.com>
2020-05-20 18:32:26 +0800

    init

---
+++ myblog-visitor-web/src/main/java/com/milosun/myblog/visitor/web/MyBlogVisitorWebApplication.java
@@ -0,0 +1,11 @@
+package com.milosun.myblog.visitor.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogVisitorWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogVisitorWebApplication.class, args);
+	}
+}
